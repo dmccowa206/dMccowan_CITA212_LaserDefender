81147cfa49e9306d2600d4fa8bb1811874ec2a77
dmccowa206 <mccowa206@morrisville.edu>
2024-10-28 13:30:11 -0400

    Set player boundaries using ViewportToWorldPoint.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,16 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     Vector2 rawInput;
-    [SerializeField] float moveSpeed = 5f;
-
-    // Update is called once per frame
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBot;
+    Vector2 minBounds, maxBounds;
+    void Start()
+    {
+        InitBounds();
+    }
     void Update()
     {
         Move();
@@ -16,13 +23,22 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBot, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
         Debug.Log(rawInput);
+    }
+    void InitBounds()
+    {
+        Camera cam = Camera.main;
+        minBounds = cam.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = cam.ViewportToWorldPoint(new Vector2(1,1));
     }
 }

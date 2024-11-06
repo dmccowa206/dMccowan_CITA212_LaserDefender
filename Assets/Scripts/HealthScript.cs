7d13482c5eb4190c359a0c9f9c88a254e114ebbe
dmccowa206 <mccowa206@morrisville.edu>
2024-11-06 13:44:04 -0500

    Implemented particle effects when enemies or the player are hit. (6.15)

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
     [SerializeField] int hp = 50;
+    [SerializeField] ParticleSystem hitFX;
     
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,6 +14,7 @@ public class HealthScript : MonoBehaviour
         if(damageDealer != null)
         {
             TakeDamage(damageDealer.GetDamage());
+            PlayHitFX();
             damageDealer.Hit();
         }
     }
@@ -21,6 +24,14 @@ public class HealthScript : MonoBehaviour
         if(hp <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+    void PlayHitFX()
+    {
+        if(hitFX != null)
+        {
+            ParticleSystem instance = Instantiate(hitFX, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
         }
     }
 }
